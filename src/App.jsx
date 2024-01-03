de2defb622827c6a2d51e2f7f7de024f4bb56646
Yagiz Avsar <63153577+AvsarYagiz@users.noreply.github.com>
2024-01-03 05:30:51 +0300

    Refactor code to add tabbed examples

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
+import { useState } from "react";
 import CoreConcept from "./components/CoreConcept";
 import Header from "./components/Header";
-import { CORE_CONCEPTS } from "./data";
+import TabButton from "./components/TabButton";
+import { CORE_CONCEPTS, EXAMPLES } from "./data";
 
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState("components");
+
+  function handleSelect(selectedButton) {
+    setSelectedTopic(selectedButton);
+  }
+
   return (
     <div>
       <Header />
@@ -11,9 +19,27 @@ function App() {
           <h2>Core Concepts</h2>
           <ul>
             {CORE_CONCEPTS.map((concept, index) => (
-              <CoreConcept concept={CORE_CONCEPTS[index]} />
+              <CoreConcept key={index} concept={CORE_CONCEPTS[index]} />
             ))}
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onClick={() => handleSelect("components")}>
+              Components
+            </TabButton>
+            <TabButton onClick={() => handleSelect("jsx")}>JSX</TabButton>
+            <TabButton onClick={() => handleSelect("props")}>Props</TabButton>
+            <TabButton onClick={() => handleSelect("state")}>State</TabButton>
+          </menu>
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>{EXAMPLES[selectedTopic].code}</code>
+            </pre>
+          </div>
         </section>
       </main>
     </div>
