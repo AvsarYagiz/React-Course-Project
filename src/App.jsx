8e8bb085d26faf5fad6ee25004be9457b06d5ae6
Yagiz Avsar <63153577+AvsarYagiz@users.noreply.github.com>
2024-01-03 04:21:27 +0300

    Moving Header and CoreConcept component into components file

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,6 @@
-import reactImg from "./assets/react-core-concepts.png";
-
+import CoreConcept from "./components/CoreConcept";
+import Header from "./components/Header";
 import { CORE_CONCEPTS } from "./data";
-
-const reactDescription = ["Fundamental", "Crucial", "Core "];
-
-function getRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-  const description = reactDescription[getRandomInt(2)];
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
-
-function CoreConcept({concept}) {
-  return (
-    <li>
-      <img src={concept.image} alt={concept.title} />
-      <h3>{concept.title}</h3>
-      <p>{concept.description}</p>
-    </li>
-  );
-} 
 
 function App() {
   return (
@@ -40,10 +10,8 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            {CORE_CONCEPTS.map((concept,index) => (
-              <CoreConcept
-                concept={CORE_CONCEPTS[index]}
-              />
+            {CORE_CONCEPTS.map((concept, index) => (
+              <CoreConcept concept={CORE_CONCEPTS[index]} />
             ))}
           </ul>
         </section>
